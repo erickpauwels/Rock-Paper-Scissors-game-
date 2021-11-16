9df91591bc6200283bfcda380c53ff47061a634f
erickpauwels <erickpauwels@hotmail.com>
2021-11-16 01:21:18 -0300

    js eventlistener

--- js/game.js
+++ js/game.js
@@ -1,1 +1,44 @@
  
+var player;
+var eva;
+
+// Player Events 
+
+rockBtn.addEventListener('click', ()=>{
+    playerResult.classList.add('rock');
+    playerResult.innerHTML = "Rock";
+    player = 1;
+    console.log(player);
+})
+
+paperBtn.addEventListener('click', ()=>{
+    playerResult.classList.add('paper');
+    playerResult.innerHTML = "Paper";
+    player = 2  
+    console.log(player)
+})
+
+scissorsBtn.addEventListener('click', ()=>{
+    playerResult.classList.add('scissors');
+    playerResult.innerHTML = "Scissors";
+    player = 3  
+    console.log(player);
+})
+
+// Eva Events
+
+playBtn.addEventListener('click', () =>{
+    eva = Math.floor(Math.random()*(3+1-1)+1);
+    
+    console.log(`soy eva con numero ${eva}`);
+    console.log(`Soy el player, ${player}`);
+    switch (eva) {
+        case (eva === 1):
+            evaResult.classList.add('rock');
+            console.log(eva);
+            break;
+        case (eva === 2)    :
+            evaResult.classList.add('paper');
+            break;
+    }
+})
---
+++ js/variables.js
@@ -0,0 +1,18 @@
+// Variables
+
+// Sart section 
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const playBtn = document.getElementById("start_btn");
+
+
+// Result Section 
+const round = document.getElementById("round");
+const playerResult = document.getElementById("player_result");
+const evaResult = document.getElementById("eva_result");
+const nextBtn = document.getElementById("next_round_btn");
+const playerCounter = document.getElementById("player_counter");
+const evaCounter = document.getElementById("eva_counter");
+
+
